f6eb2332bf7c68363fcfb1c7aacb86759d8553ba
Matthew Davis <matthewdavis111@gmail.com>
2020-05-02 15:08:17 -0400

    enable caching
    Cache requests to improve performance

--- devops-dashboard.ps1
+++ devops-dashboard.ps1
@@ -1,54 +1,55 @@
-# Get DevOps information
 $OrgName = 'matthewjdavis111'
 $PAToken = $env:PAT
 $uri = "https://dev.azure.com/$OrgName"
-$Cache:Headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAToken)")) }
+$Headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAToken)")) }
+$Init = New-UDEndpointInitialization -Variable @('OrgName', 'PAToken', 'uri', 'Headers')
 
 #region projects
 $projectUri = "$uri/_apis/projects?api-version=2.0"
-$projectList = Invoke-RestMethod -Uri $projectUri -Method Get -Headers $Cache:Headers
-$projectListSorted = $projectList.value | Sort-Object -Property name
+$projectList = Invoke-RestMethod -Uri $projectUri -Method Get -Headers $Headers
+$Cache:projectListSorted = $projectList.value | Sort-Object -Property name
 #endregion
 
-#region select
-$projectSelect = New-UDSelect -Label "Project" -Id 'projectSelect' -Option {
-    foreach ($project in $projectListSorted) {
-        New-UDSelectOption -Name $project.name -Value "$($project.id)"
-    }
+#region update build data
+$Schedule = New-UDEndpointSchedule -Every 5 -Minute
+$BuildDataRefresh = New-UDEndpoint -Schedule $Schedule -Endpoint {
+    $Cache:dataList = [System.Collections.Generic.List[pscustomobject]]::new()
     
-} -OnChange {
-    $Session:Projectid = $eventData
+    foreach ($project in $Cache:projectListSorted) {
+        $BuildURI = "$uri/$($project.id)/_apis/build/builds?api-version=5.1"
+        $buildList = Invoke-RestMethod -Uri $BuildURI -Headers $Headers
+        foreach ($build in $buildList.value) {
+            $Cache:dataList.Add(
+                [pscustomobject]@{
+                    'ProjectId'   = $project.id
+                    'BuildNumber' = $build.buildNumber
+                    'StartTime'   = $build.StartTime
+                    'FinishTime'  = $build.FinishTime
+                    'Result'      = $build.result
+                    'Commit'      = (New-UDLink -Text $($build.sourceVersion.Substring(0, 6)) -Url $($build._links.sourceVersionDisplayUri.href)) 
+                }
+            )
+        }
+    }
     Sync-UDElement -Id 'grid'
 }
 #endregion
 
-#region grid
+#region Dashboard components
+$projectSelect = New-UDSelect -Label "Project" -Id 'projectSelect' -Option {
+    foreach ($project in $cache:projectListSorted) {
+        New-UDSelectOption -Name $project.name -Value "$($project.id)"
+    }
+} -OnChange {
+    $Session:Projectid = $eventData
+    Sync-UDElement -Id 'grid'
+}
 
 $grid = New-UDGrid -Id 'grid' -Title 'Build Info' -Endpoint {
-    if ($null -eq $Session:Projectid) {
-        $Session:Projectid = $projectListSorted[0].id # needed to display build data when dashboard is first run
-    }
-
-    $BuildURI = "$uri/$Session:Projectid/_apis/build/builds?api-version=5.1"
-    $buildList = Invoke-RestMethod -Uri $BuildURI -Headers $Cache:Headers
-    $dataList = [System.Collections.Generic.list[pscustomobject]]::new()
-
-    foreach ($build in $buildList.value) {
-        $dataList.Add(
-            [pscustomobject]@{
-                'BuildNumber' = $build.buildNumber
-                'StartTime'   = $build.StartTime
-                'FinishTime'  = $build.FinishTime
-                'Result'      = $build.result
-                'Commit'      = (New-UDLink -Text $($build.sourceVersion.Substring(0, 6)) -Url $($build._links.sourceVersionDisplayUri.href)) 
-            }
-        )
-    }
-    #endregion
-    $dataList | Out-UDGridData 
+    $Cache:dataList | Where-Object -Property 'Projectid' -EQ $Session:Projectid | Out-UDGridData
 }
 #endregion
 
-$Dashboard = New-UDDashboard -Title "Build Dashboard!" -Content { $projectSelect, $grid }
-Get-UDDashboard | Stop-UDDashboard
-Start-UDDashboard -Dashboard $Dashboard -Port 10001
+
+$dashboard = New-UDDashboard -Title 'Azure DevOps Builds' -Content { $projectSelect, $grid } -EndpointInitialization $Init
+Start-UDDashboard -Dashboard $dashboard -Endpoint $BuildDataRefresh -Port 10001
