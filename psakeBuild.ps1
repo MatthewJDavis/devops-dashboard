2311aeb534611d63f347da3f373113b6697ccf08
Matthew Davis <matthewdavis111@gmail.com>
2020-05-04 19:52:03 -0400

    update psake

--- psakeBuild.ps1
+++ psakeBuild.ps1
@@ -1,11 +1,15 @@
 Task default -depends Analyse, Test
 
-Task Analyse {
+Task Analyse -description 'Analyse script with PSScriptAnalyzer' {
     'Running analyzer'
     Invoke-ScriptAnalyzer -Path .\devops-dashboard.ps1 -Verbose
 }
 
-Task Test {
+Task Test -description 'Run pester tests ' {
     'Running Unit Tests'
-    Invoke-Pester -Path $PSScriptRoot\Tests\Unit
+    $testResults = Invoke-Pester -Path $PSScriptRoot\Tests\Unit -PassThru
+    if($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error  -Message 'One or more pester test failed!'
+    }
 }
