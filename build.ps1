7081928266f719fb6693f81fd1732fdffafb7c41
Matthew Davis <matthewdavis111@gmail.com>
2020-05-04 20:09:30 -0400

    tidy pipeline add exit code

--- build.ps1
+++ build.ps1
@@ -1,13 +1,14 @@
 [cmdletbinding()]
 param(
-    [Validateset('Default','Analyse','Test')]
+    [Validateset('Default', 'Analyse', 'Test')]
     [string[]]
     $Task = 'default'
 )
 
 
 if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
-if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force}
+if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force }
 if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force }
 
 Invoke-psake -buildFile "$PSScriptRoot\psakeBuild.ps1" -taskList $Task -Verbose:$VerbosePreference
+if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }
