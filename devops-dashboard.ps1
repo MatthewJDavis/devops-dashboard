5e15a958e665b9e2cec6ad77c1f9c751f6c97184
Matthew Davis <matthewdavis111@gmail.com>
2020-05-06 18:11:28 -0400

    formatting
    added percent sign

--- devops-dashboard.ps1
+++ devops-dashboard.ps1
@@ -5,11 +5,8 @@ function Test-ForAccessToken {
 }
 
 function Start-DevOPsDashboard {
-    $OrgName = 'matthewjdavis111'
-
-
     Test-ForAccessToken
-
+    $OrgName = 'matthewjdavis111'
     $PAToken = $env:PAT
     $uri = "https://dev.azure.com/$OrgName"
     $Headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAToken)")) }
@@ -25,7 +22,6 @@ function Start-DevOPsDashboard {
     $Schedule = New-UDEndpointSchedule -Every 5 -Minute
     $BuildDataRefresh = New-UDEndpoint -Schedule $Schedule -Endpoint {
         $Cache:dataList = [System.Collections.Generic.List[pscustomobject]]::new()
-
         foreach ($project in $Cache:projectListSorted) {
             $BuildURI = "$uri/$($project.id)/_apis/build/builds?api-version=5.1"
             $buildList = Invoke-RestMethod -Uri $BuildURI -Headers $Headers
@@ -74,11 +70,20 @@ function Start-DevOPsDashboard {
 
     $card = New-UDElement -Tag div -Id "Div1" -Endpoint {
         if ($null -eq $Session:Projectid) {
-            $result = 0
-            $rate = 0
+            $result = '0'
+            $rate = '0'
         }
         $result = ($Cache:dataList | Where-Object -Property 'ProjectID' -EQ $Session:Projectid | Select-Object -property 'Result' -First 1).Result 
-
+        $resultList = ($Cache:dataList | Where-Object -Property 'ProjectID' -EQ $Session:Projectid | Select-Object -property 'Result').Result
+        $total = $resultList.count
+        if ($total -gt 0) {
+            $success = ($resultList | Group-Object | Where-Object -Property Name -eq 'succeeded').Count
+            if (-not $null -eq $Session:Projectid) {
+                $rate = "$([math]::round($success / $total * 100, 2))" + '%'
+            }
+        } else {
+            $rate = '0%'
+        }
         New-UDLayout -Columns 3 -Content {
             $backgroundColour = switch ($result) {
                 'succeeded' { 'green' }
@@ -88,24 +93,9 @@ function Start-DevOPsDashboard {
             }
             New-UDCard -Id 'statusCard' -Title 'Current Status' -BackgroundColor $backgroundColour -FontColor 'White' -Text $result
             New-UDCard -Id 'buildCount' -Title 'Build Count' -BackgroundColor $backgroundColour -FontColor 'White' -Text ($Cache:dataList | Where-Object -Property 'ProjectID' -EQ $Session:Projectid | Measure-Object ).Count 
-            New-UDCard -Id 'successRate' -Title 'Success Rate' -BackgroundColor $backgroundColour -FontColor 'White' -Content {
-                $result = ($Cache:dataList | Where-Object -Property 'ProjectID' -EQ $Session:Projectid | Select-Object -property 'Result').Result
-                $total = $result.count
-                if($total -gt 0) {
-                    $success = ($result | Group-Object | Where-Object -Property Name -eq 'succeeded').Count
-                    if (-not $null -eq $Session:Projectid) {
-                        $rate = [math]::round($success / $total * 100, 2)
-                    }
-                } else {
-                    $rate = 0
-                }
-
-                New-UDParagraph -Text $rate -Color 'White'
-            }
+            New-UDCard -Id 'successRate' -Title 'Success Rate' -BackgroundColor $backgroundColour -FontColor 'White' -Text $rate
         }
     }
-
-
     $grid = New-UDGrid -Id 'grid' -Title "Build Information" -Headers @('Build Number', 'Result', 'Commit', 'Start Time', 'Finish Time') -Properties @('BuildNumber', 'Result', 'Commit', 'StartTime', 'FinishTime') -Endpoint {
         $Cache:dataList | Where-Object -Property 'Projectid' -EQ $Session:Projectid | Out-UDGridData
     }
